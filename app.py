0d5c713e965d6874b408c369059b014452e31112
ArthurNeyer <arthur.neyer@protonmail.com>
2020-03-13 18:17:43 +0100

    Initialise Flask & Tests

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request, redirect, flash
+
+
+# App config.
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d443f27567d441f2b6176a'
+
+
+@app.route("/")
+def home():
+    return render_template('home.html.jinja')
+
+
+if __name__ == "__main__":
+    app.run()
