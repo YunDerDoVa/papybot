37304d6d48922719fadc9a8046dd910f66bf1072
ArthurNeyer <arthur.neyer@protonmail.com>
2020-03-28 18:06:53 +0100

    Parser coded

--- myparser.py
+++ myparser.py
@@ -1,8 +1,28 @@
+import re
+
+
 class Parser:
+
+    QUESTION_WORDS = [
+        "où est ",
+        "adresse de ",
+    ]
 
     def __init__(self, question):
 
         self.question = question
 
     def get_place(self):
-        pass
+
+        place = None
+
+        for word in self.QUESTION_WORDS:
+            split_1 = self.question.lower().split(word)
+
+            if len(split_1) > 1:
+                parse_1 = split_1[1]
+
+                place = parse_1.split(' ?')[0]
+                break
+
+        return place
--- test/test_parser.py
+++ test/test_parser.py
@@ -8,22 +8,30 @@ class TestParser:
     from the phrase :
         - the place """
 
-    GOOD_QUESTION = "Salut Papy ! Tu sais où est la Tour Eiffel ?"
+    GOOD_QUESTION_1 = "Salut Papy ! Tu sais où est la Tour Eiffel ?"
+    GOOD_QUESTION_2 = "Salut Papy ! Tu sais où est la Tour Eiffel ? STP"
+    GOOD_QUESTION_3 = "Salut Papy ! As-tu l'adresse de la Tour Eiffel ?"
+    GOOD_QUESTION_4 = "Où est la Tour Eiffel ?"
     BAD_QUESTION = "Salut Papy ! Comment ça va ?"
-    PLACE = "Tour Eiffel"
+    PLACE = "la tour eiffel"
 
     def test_init(self):
         """ Init Parser with the question """
 
-        parser = Parser(self.GOOD_QUESTION)
+        parser = Parser(self.GOOD_QUESTION_1)
 
-        assert parser.question == self.GOOD_QUESTION
+        assert parser.question == self.GOOD_QUESTION_1
 
     def test_get_place(self):
         """ If all is OK, Parser will return the name of the place. """
 
-        parser = Parser(self.GOOD_QUESTION)
-
+        parser = Parser(self.GOOD_QUESTION_1)
+        assert parser.get_place() == self.PLACE
+        parser = Parser(self.GOOD_QUESTION_2)
+        assert parser.get_place() == self.PLACE
+        parser = Parser(self.GOOD_QUESTION_3)
+        assert parser.get_place() == self.PLACE
+        parser = Parser(self.GOOD_QUESTION_4)
         assert parser.get_place() == self.PLACE
 
     def test_get_place_bad_question(self):
